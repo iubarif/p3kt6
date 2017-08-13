3cbfc9fb7ca619b0fd2346a8446566b91ee84c72
iubarif <iubarif@yahoo.com>
2017-08-13 14:55:55 -0700

    Removed unused assemblies

--- api.eventful.classes/SearchOption.cs
+++ api.eventful.classes/SearchOption.cs
@@ -1,9 +1,5 @@
 ﻿using api.eventful.classes.Geocode;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace api.eventful.classes
 {
