d5c1462c14ea0124a09e917937097902317785f6
iubarif <iubarif@yahoo.com>
2017-08-13 23:05:31 -0700

    Code cleanup

---
+++ api.eventful.classes/Utilities.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace api.eventful.classes
+{
+	public static class Utilities
+	{
+		/// <summary>
+		/// Extension Method for Dictionary <string, T>.
+		/// If dictionary.containsKey(key) then Remove that pair.and add new given key value pair.
+		/// </summary>
+		/// <typeparam name="T">Any type</typeparam>
+		/// <param name="dictionary">this Dictionary</param>
+		/// <param name="key">Key</param>
+		/// <param name="value">Value</param>
+		public static void AddToDictionary<T>(this Dictionary<string,T> dictionary, string key, T value ) {
+			if (dictionary.ContainsKey(key))
+				dictionary.Remove(key);
+
+			dictionary.Add(key,value);
+		}
+
+		/// <summary>
+		/// For a given SearchOption object generates Query String
+		/// </summary>
+		/// <param name="searchOption"></param>
+		/// <returns>query string</returns>
+		public static string SearchOptionToURL(SearchOption searchOption)
+		{
+			Dictionary<string, string> pocoJsonMap = Constants.POCOJsonMap;
+			Dictionary<string, string> queryStringParts = new Dictionary<string, string>();
+			Dictionary<string, string> dateStore = new Dictionary<string, string>();
+			Dictionary<string, string> geoCordinate = new Dictionary<string, string>();
+
+			var properties = typeof(SearchOption).GetProperties();
+
+			StringBuilder queryString = new StringBuilder();
+
+			foreach (var property in properties)
+			{
+				var propertyName = property.Name;
+				var propertyValue = searchOption.GetType().GetProperty(propertyName).GetValue(searchOption);
+
+				if (pocoJsonMap.ContainsKey(propertyName) && propertyValue != null)
+				{
+					if (propertyName == Constants.Lat || propertyName == Constants.Lng)
+					{
+						geoCordinate.AddToDictionary(propertyName, propertyValue.ToString());
+					}
+					else if (propertyValue.GetType() != typeof(DateTime))
+					{
+						queryStringParts.AddToDictionary(pocoJsonMap[propertyName], propertyValue.ToString());
+					}
+					else
+					{
+						DateTime date;
+
+						if (DateTime.TryParse(propertyValue.ToString(), out date))
+						{
+							dateStore.AddToDictionary(propertyName, date.ToString(Constants.DATEFormat));
+						}
+						else {
+							throw new Exception("Invalid DateTime.!!");
+						}
+					}
+				}
+			}
+
+			// Prepare Geo coordinate object. Ex:  32.746682,-117.162741 
+			if (geoCordinate.Count == 2
+				&& geoCordinate.ContainsKey(Constants.Lat)
+				&& geoCordinate.ContainsKey(Constants.Lng)
+				&& pocoJsonMap.ContainsKey(Constants.Lat)
+				)
+			{
+				var geoCords = string.Format("{0},{1}", geoCordinate[Constants.Lat], geoCordinate[Constants.Lng]);
+
+				queryStringParts.AddToDictionary(pocoJsonMap[Constants.Lat], geoCords);
+			}
+			else
+			{
+				throw new Exception("Invalid Latitude or Longitude..");
+			}
+
+			// Prepare date range YYYYMMDD-YYYYMMDD.Ex:  20170101 - 20170102
+			if (dateStore.Count == 2
+				&& dateStore.ContainsKey(Constants.DateStart) 
+				&& dateStore.ContainsKey(Constants.DateEnd)
+				&& pocoJsonMap.ContainsKey(Constants.DateStart)
+				)
+			{
+				var dateQs = string.Format("{0}-{1}", dateStore[Constants.DateStart], dateStore[Constants.DateEnd]);
+
+				queryStringParts.AddToDictionary(pocoJsonMap[Constants.DateStart], dateQs);
+			}
+			else
+			{
+				throw new Exception("Invalid dates..");
+			}
+
+			// Go through all search parameter and prepare query string
+			foreach (var pair in queryStringParts)
+			{
+				queryString.Append(string.Format("{0}{1}={2}", queryString.Length == 0 ? "" : "&", pair.Key, pair.Value));
+			}
+
+			return queryString.ToString();
+		}
+	}
+}
--- api.eventful.web/Controllers/EventfulController.cs
+++ api.eventful.web/Controllers/EventfulController.cs
@@ -2,11 +2,8 @@
 using api.eventful.classes.Eventful;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Configuration;
 using System.Web.Http;
 
@@ -23,7 +20,7 @@ namespace api.eventful.web.Controllers
 		
 		public async Task<IHttpActionResult> Post(SearchOption searchOptions) {
 
-			_serviceContext.QueryString = SearchOptionToURL(searchOptions); 
+			_serviceContext.QueryString = Utilities.SearchOptionToURL(searchOptions); 
 
 			string url = _serviceContext.ServiceEndPoint;
 
@@ -36,106 +33,6 @@ namespace api.eventful.web.Controllers
 
 				return Ok(eventfulObject);
 			}
-		}
-
-		private string SearchOptionToURL(SearchOption searchOption)
-		{
-			Dictionary<string, string> pocoJsonMap = Constants.POCOJsonMap;
-			Dictionary<string, string> queryStringParts = new Dictionary<string, string>();
-			Dictionary<string, string> dateStore = new Dictionary<string, string>();
-			Dictionary<string, string> geoCordinate = new Dictionary<string, string>();
-
-			var properties = typeof(SearchOption).GetProperties();
-
-			StringBuilder queryString = new StringBuilder();
-
-			foreach (var property in properties)
-			{
-				var propertyName = property.Name;
-				var propertyValue = searchOption.GetType().GetProperty(propertyName).GetValue(searchOption);
-
-				if (pocoJsonMap.ContainsKey(propertyName) && propertyValue != null)
-				{
-					if(propertyName == Constants.Lat || propertyName == Constants.Lng) {
-						
-						if (geoCordinate.ContainsKey(propertyName))
-							geoCordinate.Remove(propertyName);
-						else
-							geoCordinate.Add(propertyName, propertyValue.ToString());
-					}
-					else if (propertyValue.GetType() != typeof(DateTime))
-					{
-
-						var currentKey = pocoJsonMap[propertyName];
-
-						if (queryStringParts.ContainsKey(currentKey))
-							queryStringParts.Remove(currentKey);
-						else
-							queryStringParts.Add(currentKey, propertyValue.ToString());
-					}
-					else {
-						DateTime date;
-
-						if (DateTime.TryParse(propertyValue.ToString(),out date))
-						{
-							if (dateStore.ContainsKey(propertyName))
-								dateStore.Remove(propertyName);
-							else
-								dateStore.Add(propertyName, date.ToString(Constants.DATEFormat));
-						} 
-					}
-				}
-			}
-
-			// http://api.eventful.com/json/events/search?location=32.746682, -117.162741&within=1&category=books
-			if (geoCordinate.Count == 2
-				&& geoCordinate.ContainsKey(Constants.Lat)
-				&& geoCordinate.ContainsKey(Constants.Lng)
-				&& pocoJsonMap.ContainsKey(Constants.Lat)
-				)
-			{
-				var geoCords = string.Format("{0},{1}", geoCordinate[Constants.Lat], geoCordinate[Constants.Lng]);
-				var currentKey = pocoJsonMap[Constants.Lat];
-
-				if (queryStringParts.ContainsKey(currentKey))
-					queryStringParts.Remove(currentKey);
-
-				queryStringParts.Add(currentKey, geoCords);
-			}
-			else
-			{
-				throw new Exception("Invalid Latitude or Longitude..");
-			}
-
-
-
-
-			if (dateStore.Count == 2
-				&& dateStore.ContainsKey(Constants.DateStart)   // "DateStart" 
-				&& dateStore.ContainsKey(Constants.DateEnd)     //	"DateEnd"
-				&& pocoJsonMap.ContainsKey(Constants.DateStart) //	"DateStart"
-				)
-			{
-				var dateQs = string.Format("{0}-{1}", dateStore[Constants.DateStart], dateStore[Constants.DateEnd]);
-				var currentKey = pocoJsonMap[Constants.DateStart];
-
-				if (queryStringParts.ContainsKey(currentKey))
-					queryStringParts.Remove(currentKey);
-
-				queryStringParts.Add(currentKey, dateQs);
-			}
-			else
-			{
-				throw new Exception("Invalid dates..");
-			}
-
-			foreach (var pair in queryStringParts) {
-				queryString.Append(string.Format("{0}{1}={2}", queryString.Length==0?"":"&", pair.Key, pair.Value));
-			}
-
-			return queryString.ToString();
-		}
-
-		private void AddToDictionary() { }
+		}		
 	}
 }
